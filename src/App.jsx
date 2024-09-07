68d095e1d9b78b5c1595273eb4b4f66d12a7a3e6
abdulrg01 <abdulrg01@gmail.com>
2024-09-07 12:01:27 +0100

    readme

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import Features from "./components/Features";
 import HowItWorks from "./components/HowItWorks";
 import Footer from "./components/Footer";
 
-// import * as Sentry from '@sentry/react';
-
 const App = () => {
   return (
     <main className="bg-black">
@@ -23,4 +21,3 @@ const App = () => {
 };
 
 export default App;
-// export default Sentry.withProfiler(App);
